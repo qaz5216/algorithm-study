ab9162b1b07db49bea8b50295d3b1a34c23717a5
qaz5216 <70632234+qaz5216@users.noreply.github.com>
2024-03-05 17:05:29 +0900

    240305

---
+++ 백준/1992.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+int N;
+bool board[65][65];
+void QuadTree(int x,int y,int num)
+{
+    bool check=true; 
+    for(int i=x;i<x+num;i++)
+        for(int j=y;j<y+num;j++)
+        {
+            if(board[x][y]!=board[i][j])
+            {
+                check=false;
+                break;
+            }
+        }
+    if(!check)
+    {
+        cout<<"(";
+        QuadTree(x,y,num/2);
+        QuadTree(x,y+num/2,num/2);
+        QuadTree(x+num/2,y,num/2);
+        QuadTree(x+num/2,y+num/2,num/2);
+        cout<<")";
+    }
+    else
+    {
+        cout<<board[x][y];
+    }
+    check=true;
+}
+
+
+int main()
+{
+    cin >> N;
+    char c;
+    for(int i=0;i<N;i++)
+        for(int j=0;j<N;j++){
+            cin>>c;
+            board[i][j]=c-'0';    
+        }
+
+    QuadTree(0,0,N);
+    return 0;
+}
---
+++ 백준/2630.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool board[128][128]; // true 파랑 false 하양
+int N;
+int pa,ha;
+
+void cal(int x,int y,int num)
+{
+    if(num==1)
+    {
+        if(board[x][y])
+            pa+=1;
+        else
+            ha+=1;
+        return;
+    }
+    bool flag1=true;
+    for(int i=x;i<x+num;i++)
+    {
+        for(int j=y;j<y+num;j++)
+        {
+            if(board[i][j]!=board[x][y])
+                flag1=false;
+        }
+    }
+    if(flag1)
+    {
+        if(board[x][y])
+            pa+=1;
+        else
+            ha+=1;
+    }
+    else
+    {
+        cal(x,y,num/2);
+        cal(x,y+(num/2),num/2);
+        cal(x+(num/2),y,num/2);
+        cal(x+(num/2),y+(num/2),num/2);
+    }
+}
+
+int main()
+{
+    cin >> N;
+    for(int i=0;i<N;i++)
+    {
+        for(int j=0;j<N;j++){
+            cin>>board[i][j];
+        }
+            
+    }
+    pa=0;
+    ha=0;
+    cal(0,0,N);
+    cout<<ha<<"\n"<<pa;
+    return 0;
+}
