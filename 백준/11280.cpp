e25462d787d7b016bda7bef68622dca6c5ac8ad8
qaz5216 <70632234+qaz5216@users.noreply.github.com>
2024-04-23 14:39:59 +0900

    0423

---
+++ 백준/11280.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+#define MN 10001
+
+int N,M;
+vector<int> g[MN*2];
+vector<int> rg[MN*2];
+vector<int> st;
+bool visit[MN*2];
+vector<vector<int>>ssc;
+int ssclayer[MN*2];
+
+int notx(int x)
+{
+    return x>N ? x-N:x+N;
+}
+
+void dfs(int n)
+{
+    visit[n]=true;
+    for(int next:g[n])
+    {
+        if(visit[next]) continue;
+        dfs(next);
+    }
+    st.push_back(n);
+}
+
+void rdfs(int n)
+{
+    visit[n]=true;
+    for(int next:rg[n])
+    {
+        if(visit[next]) continue;
+        rdfs(next);
+    }
+    ssc.back().push_back(n);
+    ssclayer[n]=ssc.size();
+}
+
+int main()
+{
+    cin>>N>>M;
+    for(int i=0;i<M;i++)
+    {
+        int a,b;
+        cin >> a >> b;
+        if(a<0)a=-a+N;
+        if(b<0)b=-b+N;
+        g[notx(a)].push_back(b);
+        g[notx(b)].push_back(a);
+        rg[b].push_back(notx(a));
+        rg[a].push_back(notx(b));
+    }
+    for(int i=1;i<=N;i++)
+    {
+        if(visit[i]) continue;
+        dfs(i);
+    }
+    memset(visit,false,sizeof(visit));
+    reverse(st.begin(),st.end());
+    for(int n:st)
+    {
+        if(visit[n]) continue;
+        ssc.push_back(vector<int>());
+        rdfs(n);
+    }
+    for(int i=1;i<=N;i++)
+    {
+        if(ssclayer[i]==ssclayer[i+N])
+        {
+            cout<<0;
+            return 0;
+        }
+    }
+    cout<<1;
+    return 0;
+}
---
+++ 백준/2207.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+const int MN = 20002;
+vector<int> g[MN], rg[MN], st;
+vector<vector<int> > scc;
+bool vst[MN];
+
+int N, M;
+
+int Not(int a) {
+    if(a > M) return a - M;
+    else return a + M;
+}
+
+void dfs(int n) {
+    for(int nxt : g[n]) {
+        if(vst[nxt]) continue;
+        vst[nxt] = true;
+        dfs(nxt);
+    }
+
+    st.push_back(n);
+}
+
+void dfs2(int n) {
+    for(int nxt : rg[n]) {
+        if(vst[nxt]) continue;
+        vst[nxt] = true;
+        dfs2(nxt);
+    }
+
+    scc.back().push_back(n);
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    cin >> N >> M;
+    for(int i = 0; i < N; i++) {
+        int a, b; cin >> a >> b;
+        if(a < 0) a = -a + M;
+        if(b < 0) b = -b + M;
+        g[Not(a)].push_back(b);
+        g[Not(b)].push_back(a);
+        rg[a].push_back(Not(b));
+        rg[b].push_back(Not(a));
+    }
+
+    for(int i = 1; i <= M * 2; i++) {
+        if(vst[i]) continue;
+        vst[i] = true;
+        dfs(i);
+    }
+
+    reverse(st.begin(), st.end());
+    memset(vst, 0, sizeof(vst));
+
+    for(int n : st) {
+        if(vst[n]) continue;
+        vst[n] = true;
+        scc.push_back(vector<int>());
+        dfs2(n);
+    }
+
+    memset(vst, 0, sizeof(vst));
+
+    for(vector<int> i : scc) {
+        for(int j : i) {
+            if(vst[Not(j)]) {
+                cout << "OTL";
+                return 0;
+            }
+            vst[j] = true;
+        }
+        memset(vst, 0, sizeof(vst));
+    }
+
+    cout << "^_^";
+}
