3356f4bdb2938e9aa1fcee06de57204875102583
qaz5216 <70632234+qaz5216@users.noreply.github.com>
2024-05-02 18:13:24 +0900

    240502

---
+++ 백준/19598.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int N;
+vector<pair<int,int>> vec;
+priority_queue<int,vector<int>,greater<int>> pq;
+bool cmp(pair<int,int> a,pair<int,int> b)
+{
+    return a.first<b.first;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> N;
+    for(int i=0;i<N;i++)
+    {
+        int s,e;
+        cin>>s>>e;
+        vec.push_back({s,e});
+    }
+    sort(vec.begin(),vec.end());
+    int ans=1;
+    int idx=0;
+    pq.push(vec[0].second);
+    for(int i=1;i<vec.size();i++)
+    {
+        if(pq.top()<=vec[i].first)
+        {
+            pq.push(vec[i].second);
+            pq.pop();
+        }
+        else
+        {
+            pq.push(vec[i].second);
+        }
+    }
+    cout<<pq.size();
+    return 0;
+}
