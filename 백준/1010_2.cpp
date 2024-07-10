71811ba68d79e78222bee6ab654b21018896b43d
Park-Kyoung-Min <70632234+qaz5216@users.noreply.github.com>
2024-07-10 18:47:45 +0900

    240710

---
+++ 백준/1010_2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int T;
+    int N,M;
+    cin>>T;
+    while (T--)
+    {
+        long long ans=1;
+        cin>>N>>M;
+        //cout<<mCn
+        for(int i=1;i<=N;i++)
+        {
+            ans*=M;
+            M--;
+            ans/=i;
+        }
+        cout<<ans<<"\n";
+    }
+    return 0;
+}
